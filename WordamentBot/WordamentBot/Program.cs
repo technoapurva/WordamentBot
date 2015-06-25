20bd462d780f361f5b2144b1bf733b18ec4fafa0
Apurva Pathak <technoapurva@gmail.com>
2015-06-25 22:52:26 +0530

    Complete Wordament bot

--- WordamentBot/WordamentBot/Program.cs
+++ WordamentBot/WordamentBot/Program.cs
@@ -1,70 +1,135 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace WordamentBot
 {
     class Program
     {
         static Dictionary<string, bool> wordList = new Dictionary<string, bool>();
-        static List<string> validWords=new List<string>();
+        static Dictionary<string, List<int>> validWords = new Dictionary<string, List<int>>();
         static string[,] game = new string[,]{
-            {"E","S","N","I"},
-            {"O","L","T","E"},
-            {"Z","E","A","S"},
-            {"N","D","E","N"}
+            {"I","R","S","A"},
+            {"I","N","E","I"},
+            {"G","N","N","D"},
+            {"M","U","R","O"}
         };
+
+        static int baseX = 225;
+        static int baseY = 220;
+        static int incrementX = 150;
+        static int incrementY = 150;
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        static extern bool SetCursorPos(int x, int y);
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        public const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        public const int MOUSEEVENTF_LEFTUP = 0x04;
+
+        //This simulates a left mouse click
+        public static void LeftMouseClick(int xpos, int ypos)
+        {
+            SetCursorPos(xpos, ypos);
+            mouse_event(MOUSEEVENTF_LEFTDOWN, xpos, ypos, 0, 0);
+            //  mouse_event(MOUSEEVENTF_LEFTUP, xpos, ypos, 0, 0);
+        }
+
+        private static void InitPixels()
+        {
+            Rectangle resolution = Screen.PrimaryScreen.Bounds;
+            double multiplierX = resolution.Size.Width / 1600;
+            double multiplierY = resolution.Size.Height / 900;
+            baseX = (int)(multiplierX * baseX);
+            baseY = (int)(multiplierY * baseY);
+            incrementX = (int)(incrementX * baseX);
+            incrementY = (int)(incrementY * baseY);
+        }
         private static void LoadDictionary()
         {
             File.ReadAllLines(@"..\..\Files\Words.txt").Distinct().Where(t => t.Length >= 3).ToList().ForEach(word => wordList.Add(word.ToUpper(), true));
-        } 
+        }
         static bool IsValidPosition(int pos)
         {
-            return (pos>=0 && pos<4);
+            return (pos >= 0 && pos < 4);
         }
         static void WordamentCrack(string currentString, int currentPosition, List<int> coveredLetters)
         {
-            int row = currentPosition / 10;
-            int col = currentPosition % 10;
-            if(wordList.ContainsKey(currentString))
+            if (String.IsNullOrEmpty(currentString))
             {
-                validWords.Add(currentString);
-            }
-            for(int r=row-1;r<=row+1;r++)
-            {
-                for(int c=col-1;c<=col+1;c++)
+                for (int i = 0; i < 4; i++)
                 {
-                    if(r==row && c==col)
+                    for (int j = 0; j < 4; j++)
                     {
-                        continue;
-                    }
-                    int posToStore=r*10+c;
-                    if(IsValidPosition(r) && IsValidPosition(c) && !coveredLetters.Contains(posToStore))
-                    {
-                        List<int> covLetters=new List<int>();
-                        coveredLetters.ForEach(t => covLetters.Add(t));
-                        covLetters.Add(posToStore);
-                        WordamentCrack(currentString + game[r, c], posToStore, covLetters);
+                        WordamentCrack(game[i, j], i * 10 + j, new List<int>() { i * 10 + j });
                     }
                 }
+            }
+            else
+            {
+                int row = currentPosition / 10;
+                int col = currentPosition % 10;
+                if (wordList.ContainsKey(currentString) && !validWords.ContainsKey(currentString))
+                {
+                    validWords.Add(currentString, coveredLetters);
+                }
+                for (int r = row - 1; r <= row + 1; r++)
+                {
+                    for (int c = col - 1; c <= col + 1; c++)
+                    {
+                        if (r == row && c == col)
+                        {
+                            continue;
+                        }
+                        int posToStore = r * 10 + c;
+                        if (IsValidPosition(r) && IsValidPosition(c) && !coveredLetters.Contains(posToStore))
+                        {
+                            List<int> covLetters = new List<int>();
+                            coveredLetters.ForEach(t => covLetters.Add(t));
+                            covLetters.Add(posToStore);
+                            WordamentCrack(currentString + game[r, c], posToStore, covLetters);
+                        }
+                    }
+                }
+            }
+        }
+        static void BotInAction()
+        {
+            foreach (KeyValuePair<string, List<int>> keyValue in validWords)
+            {
+                int x = 0, y = 0;
+                for(int pos=0;pos<keyValue.Value.Count;pos++)
+                {
+                    y = keyValue.Value[pos] / 10;
+                    x = keyValue.Value[pos] % 10;
+                    LeftMouseClick(baseX + incrementX * x, baseY + incrementY * y);
+                    Thread.Sleep(30);
+                }
+                mouse_event(MOUSEEVENTF_LEFTUP, 225 + 150 * x, 220 + 150 * y, 0, 0);
+                Thread.Sleep(30);
             }
         }
         static void Main(string[] args)
         {
+           // InitPixels();
+            //  Thread.Sleep(3000);
+            //  LeftMouseClick(225 , 220);
+            //  LeftMouseClick(375, 220);
+            //  LeftMouseClick(525, 370);
+            ////  LeftMouseClick(375, 220);
+            //  Thread.Sleep(100000);
             LoadDictionary();
-            for (int i = 0; i < 4;i++ )
-            {
-                for(int j=0;j<4;j++)
-                {
-                    WordamentCrack(game[i,j], i * 10 + j, new List<int>() { i * 10 + j });
-                }
-            }
-            validWords = validWords.Distinct().OrderByDescending(t=>t.Length).ToList();
-            Console.Read();
+            WordamentCrack(String.Empty, 0, null);
+            BotInAction();
         }
     }
 }
